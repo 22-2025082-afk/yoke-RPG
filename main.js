e54fb66404e66f2ab929d5b81faa292b3ec92a4d
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-31 09:59:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const canvas = document.getElementById("gameCanvas");
-
 const ctx = canvas.getContext("2d");
-
 
 canvas.width = 640;
 canvas.height = 360;
 
 
-startGame(canvas, ctx);
+ctx.fillStyle = "red";
+ctx.fillRect(100,100,100,100);
