5a4c2808462eee06d3b9f54378ad487e4636c99c
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-31 09:49:01 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,22 @@
+const player = {
 
+    x: 25,
+    y: 25,
+
+    size: 16
+};
+
+
+
+function drawPlayer(ctx){
+
+    ctx.fillStyle = "blue";
+
+
+    ctx.fillRect(
+        player.x * TILE_SIZE,
+        player.y * TILE_SIZE,
+        player.size,
+        player.size
+    );
+}
