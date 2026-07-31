cd719de6dca325cc93a53bcea18148f35a8fe883
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-31 09:48:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,28 +4,4 @@ const ctx = canvas.getContext("2d");
 canvas.width = 640;
 canvas.height = 360;
 
-
-function gameLoop(){
-
-    ctx.clearRect(
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
-
-
-    ctx.fillStyle = "green";
-    ctx.fillRect(
-        0,
-        0,
-        canvas.width,
-        canvas.height
-    );
-
-
-    requestAnimationFrame(gameLoop);
-}
-
-
-gameLoop();
+startGame(canvas, ctx);
