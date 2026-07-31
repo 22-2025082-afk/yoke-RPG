7b78c186c68280579b679c3780bf719bc5c2531d
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-31 09:48:32 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,29 @@
+let canvas;
+let ctx;
 
+
+function startGame(c, context){
+
+    canvas = c;
+    ctx = context;
+
+    gameLoop();
+}
+
+
+function gameLoop(){
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    drawMap(ctx);
+    drawPlayer(ctx);
+
+
+    requestAnimationFrame(gameLoop);
+}
